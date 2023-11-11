341ab61947fa98f01be0ad7cb1e0b33923e35ad7
Melketsedek Cheru <106968143+san-21@users.noreply.github.com>
2023-11-11 21:41:21 +0300

    Update axiosClient.js

--- src/services/axiosClient.js
+++ src/services/axiosClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 import { logout, setToken } from "../redux-state/auth/authReducer";
 export const instance = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "https://mernjwt-api.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
